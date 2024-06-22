211deb698e8bf7a436c17f4a52268e2a2192f8ad
wvwvw5 <e.kononykhin@mail.ru>
2024-06-23 02:07:24 +0300

    [eq

---
+++ View/Appointment_Doc_White.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+namespace EMIAS.View
+{
+    /// <summary>
+    /// Логика взаимодействия для Appointment_Doc_White.xaml
+    /// </summary>
+    public partial class Appointment_Doc_White : Window
+    {
+
+        public Appointment_Doc_White()
+        {
+            InitializeComponent();
+        }
+        private void OnMainMenuButtonClick(object sender, RoutedEventArgs e)
+        {
+            MainMenuStackPanel.Visibility = MainMenuStackPanel.Visibility == Visibility.Collapsed ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        private void OnMedCardButtonClick(object sender, RoutedEventArgs e)
+        {
+            MedCardStackPanel.Visibility = MedCardStackPanel.Visibility == Visibility.Collapsed ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        private void OnIrinaButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (IrinaMenuStackPanel.Visibility == Visibility.Collapsed)
+            {
+                IrinaMenuStackPanel.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                IrinaMenuStackPanel.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void OnGearButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (OnGearStackPanel.Visibility == Visibility.Collapsed)
+            {
+                OnGearStackPanel.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                OnGearStackPanel.Visibility = Visibility.Collapsed;
+            }
+        }
+
+
+        private void OnExitClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            // Обработчик выхода
+            MessageBox.Show("Выход");
+        }
+
+        private void OnDarkThemeClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            Appointment_Doc_Black white = new Appointment_Doc_Black();
+            white.Show();
+            Close();
+
+        }
+    }
+}
+
+
+
+
+
---
+++ ViewModel/AppointmentDocWhiteViewModel.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Media;
+
+namespace EMIAS.ViewModel
+{
+    public class AppointmentDocWhiteViewModel : INotifyPropertyChanged
+    {
+        private double _buttonWidth = 97;
+        private double _buttonHeight = 80;
+        private double _stackPanelWidth = 80;
+        private double _stackPanelHeight = 50;
+        private double _imageHeight = 30;
+        private double _textSize = 10;
+        private Brush _buttonColor = Brushes.White;
+        private Brush _backgroundColor = Brushes.LightGray;
+        private Brush _scrollViewerColor = Brushes.LightGray;
+        private Brush _scrollBarColor = Brushes.White;
+
+        public double ButtonWidth
+        {
+            get { return _buttonWidth; }
+            set
+            {
+                if (_buttonWidth != value)
+                {
+                    _buttonWidth = value;
+                    OnPropertyChanged(nameof(ButtonWidth));
+                }
+            }
+        }
+
+        public double ButtonHeight
+        {
+            get { return _buttonHeight; }
+            set
+            {
+                if (_buttonHeight != value)
+                {
+                    _buttonHeight = value;
+                    OnPropertyChanged(nameof(ButtonHeight));
+                }
+            }
+        }
+
+        public double StackPanelWidth
+        {
+            get { return _stackPanelWidth; }
+            set
+            {
+                if (_stackPanelWidth != value)
+                {
+                    _stackPanelWidth = value;
+                    OnPropertyChanged(nameof(StackPanelWidth));
+                }
+            }
+        }
+
+        public double StackPanelHeight
+        {
+            get { return _stackPanelHeight; }
+            set
+            {
+                if (_stackPanelHeight != value)
+                {
+                    _stackPanelHeight = value;
+                    OnPropertyChanged(nameof(StackPanelHeight));
+                }
+            }
+        }
+
+        public double ImageHeight
+        {
+            get { return _imageHeight; }
+            set
+            {
+                if (_imageHeight != value)
+                {
+                    _imageHeight = value;
+                    OnPropertyChanged(nameof(ImageHeight));
+                }
+            }
+        }
+
+        public double TextSize
+        {
+            get { return _textSize; }
+            set
+            {
+                if (_textSize != value)
+                {
+                    _textSize = value;
+                    OnPropertyChanged(nameof(TextSize));
+                }
+            }
+        }
+
+        public Brush ButtonColor
+        {
+            get { return _buttonColor; }
+            set
+            {
+                if (_buttonColor != value)
+                {
+                    _buttonColor = value;
+                    OnPropertyChanged(nameof(ButtonColor));
+                }
+            }
+        }
+
+        public Brush BackgroundColor
+        {
+            get { return _backgroundColor; }
+            set
+            {
+                if (_backgroundColor != value)
+                {
+                    _backgroundColor = value;
+                    OnPropertyChanged(nameof(BackgroundColor));
+                }
+            }
+        }
+
+        public Brush ScrollViewerColor
+        {
+            get { return _scrollViewerColor; }
+            set
+            {
+                if (_scrollViewerColor != value)
+                {
+                    _scrollViewerColor = value;
+                    OnPropertyChanged(nameof(ScrollViewerColor));
+                }
+            }
+        }
+
+        public Brush ScrollBarColor
+        {
+            get { return _scrollBarColor; }
+            set
+            {
+                if (_scrollBarColor != value)
+                {
+                    _scrollBarColor = value;
+                    OnPropertyChanged(nameof(ScrollBarColor));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
