abadaeceffd356dca303054f3b29ca48c1a0db32
MatveyTolstov <144270275+MatveyTolstov@users.noreply.github.com>
2024-06-23 15:47:31 +0300

    Сделал класс

---
+++ ViewModel/Helpers/ApiHelper.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMIAS.ViewModel.Helpers
+{
+    internal class ApiHelper
+    {
+        private static string Url = "https://localhost:7280/swagger/index.html";
+
+        //метод для обновления данных передаем id и данные
+
+        public static string Put(string json, int id)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
+                HttpResponseMessage message = client.PutAsync(Url + "/" + id, content).Result;
+                return message.Content.ReadAsStringAsync().Result;
+
+            }
+            catch (Exception ex)
+            {
+
+                return ex.Message;
+            }
+        }
+
+        //метод для получение данных 
+
+        public static string Get()
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                HttpResponseMessage message = client.GetAsync(Url).Result;
+                return message.Content.ReadAsStringAsync().Result;
+            }
+            catch (Exception ex)
+            {
+
+                return ex.Message;
+            }
+        }
+
+        //метод для обновления данных передаем id 
+
+        public static string GetByID(int id)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                HttpResponseMessage message = client.GetAsync(Url + "/" + id).Result;
+                return message.Content.ReadAsStringAsync().Result;
+            }
+            catch (Exception ex)
+            {
+
+                return ex.Message;
+            }
+
+        }
+
+        //метод для создания данных передаем данные
+
+        public static string Post(string json)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
+                HttpResponseMessage message = client.PostAsync(Url, content).Result;
+                return message.Content.ReadAsStringAsync().Result;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        //метод для удаления данных
+
+        public static string Delete(int id)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                HttpResponseMessage message = client.DeleteAsync(Url + "/" + id).Result;
+                return message.Content.ReadAsStringAsync().Result;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+
+            }
+        }
+    }
+}
